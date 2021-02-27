310d44713a1637ac9d8b5256146876173b43eb7d
Maicon Almeida Lemos <maikiller-a7x@outlook.com>
2021-02-27 14:34:29 -0300

    work with for verbs

---
+++ RestWithAPI/Controllers/PersonCalculatorController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(firstNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(firstNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string srtNumber)
+        {
+            double number;
+            bool IsNumber = double.TryParse(srtNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return IsNumber;
+        }
+
+        private int ConvertToDecimal(string srtNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(srtNumber, out decimalValue))
+            {
+                return (int)decimalValue;
+            }
+            return 0;
+        }
+    }
+}
---
+++ RestWithAPI/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAPI.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public long firstName { get; set; }
+        public long LastName { get; set; }
+        public long Adress { get; set; }
+        public long Gender { get; set; }
+    }
+}
